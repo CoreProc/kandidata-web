621c4066f05e3678509fb8cec57f61190e859582
Jonnel Ivan Bergonia <ivan.bergonia@gmail.com>
2016-04-16 22:33:09 +0800

    Update CandidateSeeder

--- database/seeds/CandidateSeeder.php
+++ database/seeds/CandidateSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use KandiData\Candidate;
 
 class CandidateSeeder extends Seeder
 {
@@ -11,6 +12,58 @@ class CandidateSeeder extends Seeder
      */
     public function run()
     {
-        
+        DB::table('candidates')->truncate();
+
+        $candidates = [
+            [
+                "first_name" => "Jejomar",
+                "last_name" => "Binay, Sr.",
+                "middle_name" => "Cabauatan",
+                "hashtag" => "#binay",
+                "twitter_mention" => "@VPJojoBinay",
+                "position_id" => 1,
+                "period_id" => 1
+            ],
+            [
+                "first_name" => "Miriam",
+                "last_name" => "Defensor-Santiago",
+                "middle_name" => "Palma",
+                "hashtag" => "#miriam2016",
+                "twitter_mention" => "@senmiriam",
+                "position_id" => 1,
+                "period_id" => 1
+            ],
+            [
+                "first_name" => "Rodrigo",
+                "last_name" => "Duterte",
+                "middle_name" => "Roa",
+                "hashtag" => "#du30",
+                "twitter_mention" => "@RRD_Davao",
+                "position_id" => 1,
+                "period_id" => 1
+            ],
+            [
+                "first_name" => "Mary Grace",
+                "last_name" => "Llamanzares",
+                "middle_name" => "Poe",
+                "hashtag" => "#Grace2016",
+                "twitter_mention" => "@SenGracePOE",
+                "position_id" => 1,
+                "period_id" => 1
+            ],
+            [
+                "first_name" => "Manuel",
+                "last_name" => "Roxas II",
+                "middle_name" => "Araneta",
+                "hashtag" => "#roxas",
+                "twitter_mention" => "@MARoxas",
+                "position_id" => 1,
+                "period_id" => 1
+            ]
+        ];
+
+        foreach ($candidates as $candidate) {
+            Candidate::create($candidate);
+        }
     }
 }
