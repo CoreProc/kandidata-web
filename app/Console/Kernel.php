5dcc039e939f47aad1d0b1593155ee7591362015
awkwardusername <awkwardusername@gmail.com>
2016-04-17 03:04:59 +0800

    we cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,10 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                  ->hourly();
+        $schedule->command('kandidata:tweet-collector')->withoutOverlapping()
+                  ->everyThirtyMinutes();
+
+        $schedule->command('kandidata:tweet-analyzer')->withoutOverlapping()
+            ->hourly();
     }
 }
