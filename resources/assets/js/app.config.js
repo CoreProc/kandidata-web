d983e996bd32260b2529ea2180891f80977ee4b7
Jonnel Ivan Bergonia <ivan.bergonia@gmail.com>
2016-04-17 13:39:18 +0800

    Add encodeURIComponent filter

--- resources/assets/js/app.config.js
+++ resources/assets/js/app.config.js
@@ -1,20 +1,24 @@
 // App Config
 
-angular
-	.module('kandidata', [
-		'ngAnimate',
-		'angularMoment',
-		'ui.router',
-		'oc.lazyLoad'
-	]);
+angular.module('kandidata', [
+        'ngAnimate',
+        'angularMoment',
+        'ui.router',
+        'oc.lazyLoad'
+    ]);
 
 angular.module('kandidata')
-	.run([
-		'$rootScope',
-		'$state',
-		'$stateParams',
-		function ($rootScope, $state, $stateParams) {
-	        $rootScope.$state = $state;
-	        $rootScope.$stateParams = $stateParams;
-    	}
+    .filter('encodeURIComponent', function () {
+        return window.encodeURIComponent;
+    });
+
+angular.module('kandidata')
+    .run([
+        '$rootScope',
+        '$state',
+        '$stateParams',
+        function ($rootScope, $state, $stateParams) {
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }
     ]);
