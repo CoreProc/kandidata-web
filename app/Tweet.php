944ec684635a7c9e8d08aa7250765e17b270c45b
awkwardusername <awkwardusername@gmail.com>
2016-04-17 12:41:54 +0800

    set twitter_ident to string

--- app/Tweet.php
+++ app/Tweet.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tweet extends Model {
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    protected $casts = [
+        'twitter_ident' => 'string'
+    ];
+
     public function keywords()
     {
         return $this->hasMany(Keyword::class);
