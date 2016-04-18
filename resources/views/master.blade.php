89daec134ef86b92c53e3a3a2386cdefd1082616
awkwardusername <awkwardusername@gmail.com>
2016-04-19 02:37:47 +0800

    add root

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,41 +8,41 @@
 
     <meta name="description" content="A data-driven webapp for Twitter metrics for the Philippines Election 2016 presidential candidates.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="assets/img/kandidata-favicon.png">
-    <link rel="apple-touch-icon" href="assets/img/kandidata-favicon.png">
+    <link rel="icon" type="image/png" href="/assets/img/kandidata-favicon.png">
+    <link rel="apple-touch-icon" href="/assets/img/kandidata-favicon.png">
 
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css">
 
     <!-- Plugin Stylesheets -->
-    <link rel="stylesheet" href="assets/lib/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/lib/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/lib/font-awesome/css/font-awesome.min.css">
 
     <!-- Project Stylesheets -->
-    <link rel="stylesheet" href="assets/css/addon.css">
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/addon.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 
     <!-- Plugin Scripts -->
-    <script src="assets/lib/jquery/dist/jquery.min.js"></script>
-    <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/lib/angular/angular.min.js"></script>
-    <script src="assets/lib/angular-ui-router/release/angular-ui-router.min.js"></script>
-    <script src="assets/lib/angular-animate/angular-animate.min.js"></script>
-    <script src="assets/lib/angular-moment/angular-moment.min.js"></script>
-    <script src="assets/lib/moment/min/moment.min.js"></script>
-    <script src="assets/lib/amcharts/dist/amcharts/amcharts.js"></script>
-    <script src="assets/lib/oclazyload/dist/ocLazyLoad.min.js"></script>
+    <script src="/assets/lib/jquery/dist/jquery.min.js"></script>
+    <script src="/assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/assets/lib/angular/angular.min.js"></script>
+    <script src="/assets/lib/angular-ui-router/release/angular-ui-router.min.js"></script>
+    <script src="/assets/lib/angular-animate/angular-animate.min.js"></script>
+    <script src="/assets/lib/angular-moment/angular-moment.min.js"></script>
+    <script src="/assets/lib/moment/min/moment.min.js"></script>
+    <script src="/assets/lib/amcharts/dist/amcharts/amcharts.js"></script>
+    <script src="/assets/lib/oclazyload/dist/ocLazyLoad.min.js"></script>
 
     <!-- Project Scripts -->
-    <script src="assets/js/app.js"></script>
+    <script src="/assets/js/app.js"></script>
 
 </head>
 <body>
 
-<header class="header" x-ng-include="'tpl/sections/header.html'"></header>
+<header class="header" x-ng-include="'/tpl/sections/header.html'"></header>
 
 <div class="main-wrapper" ui-view> </div>
 
-<footer class="footer" x-ng-include="'tpl/sections/footer.html'"></footer>
+<footer class="footer" x-ng-include="'/tpl/sections/footer.html'"></footer>
 
 <div class="scroll-top">
     <a href="#">
